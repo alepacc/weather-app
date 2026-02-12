a98822cb6bd6c900f8659a23fbe98b96b38d7cb6
alepacc <alessiopacconi@gmail.com>
2026-02-12 11:32:53 +0100

    style: styling adjustments

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@ import Search from "./components/Search";
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen justify-center font-sans"> 
+    <div className="container">
     <Header/>
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center  py-4 px-2 sm:items-start">
+      <main>
         <h1 className="text-5xl font-display text-center">How's the sky looking today?</h1>
         <Search/>
       </main>
