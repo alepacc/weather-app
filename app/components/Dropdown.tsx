9631e922fb5272345af3f1dce3edfead0d4c7f38
alepacc <alessiopacconi@gmail.com>
2026-03-04 16:11:34 +0100

    feat: add PostCSS configuration and update Dropdown component imports

--- app/components/Dropdown.tsx
+++ app/components/Dropdown.tsx
@@ -1,7 +1,6 @@
 'use client';
-import Image from "next/image";
 import { useState } from "react";
-import { ChevronDown, ChevronUp, Zap } from "lucide-react";
+import { ChevronDown, ChevronUp } from "lucide-react";
 
 type DropdownProps = {
   label: string;
